73dc351e2174fd4377f5819b4c074f8bcb0504ef
femi <cocofemi@gmail.com>
2025-06-01 02:44:27 +0100

    changes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dbConnect = require("./db/dbConnect");
 const dotenv = require("./dotenvConfig")();
 const Cors = require("cors");
+const cron = require("node-cron");
 const webhookRoutes = require("./routes/webhook");
 
 dbConnect();
@@ -23,3 +24,14 @@ app.use(express.urlencoded({ extended: true }));
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.use("/webhook", webhookRoutes);
+
+cron.schedule("*/13 * * * *", async () => {
+  try {
+    const response = await axios.get(
+      "https://contract-app-backend.onrender.com"
+    );
+    console.log(`Health check response: ${response.status}`);
+  } catch (error) {
+    console.error(`Health check error: ${error.message}`);
+  }
+});
